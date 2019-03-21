13517ddbcf172a6a08b540bd3c2efde002f4926e
younesbenmammar <younescomptepro@gmail.com>
2019-03-21 10:09:54 +0100

    Association de la base de donnée au template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # recupere la liste des poste par date de publication 
+    return render(request, 'blog/post_list.html', {'posts':posts})
